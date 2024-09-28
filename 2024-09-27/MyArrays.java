e64ddb30d8b25bedec2beb9e9f2f8fb149617d3c
Anzam Siddek <asiddek50@stuy.edu>
2024-09-27 21:16:51 -0400

    returnCopy finished

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,8 +1,32 @@
 public class MyArrays{
   public static void main(String[] args) {
     //testing aryToString
-    int[] ary = {1, 2, 3, 4, 5};
+    int[] ary = {11, 28, 13, 94, 45};
     System.out.println(aryToString(ary));
+
+    //testing returnCopy
+    System.out.println("Original: " + aryToString(ary) + " Copy: " + aryToString(returnCopy(ary)));
+    System.out.println("Original address " + ary + " Copy address: " + returnCopy(ary));
+    int[] ary2 = {};
+    System.out.println("Original: " + aryToString(ary2) + " Copy: " + aryToString(returnCopy(ary2)));
+    System.out.println("Original address " + ary2 + " Copy address: " + returnCopy(ary2));
+    int[] ary3 = {1};
+    System.out.println("Original: " + aryToString(ary3) + " Copy: " + aryToString(returnCopy(ary3)));
+    System.out.println("Original address " + ary3 + " Copy address: " + returnCopy(ary3));
+  }
+
+  public static int[] returnCopy(int[]ary) {
+    if (ary.length == 0) {
+      return new int[0];
+    }
+
+    int[] res = new int[ary.length];
+
+    for (int x = 0; x < ary.length; x++) {
+      res[x] = ary[x];
+    }
+
+    return res;
   }
 
   public static String aryToString(int[] nums) {
