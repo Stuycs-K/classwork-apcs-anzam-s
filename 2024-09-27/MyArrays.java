0e3608ca7392a6644ffee4b7334771788fb031b9
Anzam Siddek <asiddek50@stuy.edu>
2024-09-27 21:53:31 -0400

    concatArray completed

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -13,6 +13,33 @@ public class MyArrays{
     int[] ary3 = {1};
     System.out.println("Original: " + aryToString(ary3) + " Copy: " + aryToString(returnCopy(ary3)));
     System.out.println("Original address " + ary3 + " Copy address: " + returnCopy(ary3));
+
+    //testing concatArray
+    System.out.println("Array 1: " + aryToString(ary) + " Array 2: " + aryToString(ary2));
+    System.out.println("Concatenated Array: " + aryToString(concatArray(ary, ary2)));
+    System.out.println("Array 1: " + aryToString(ary3) + " Array 2: " + aryToString(ary));
+    System.out.println("Concatenated Array: " + aryToString(concatArray(ary3, ary)));
+  }
+
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+    int[] res = new int[ary1.length + ary2.length];
+    int counter = 0;
+
+    if (ary1.length != 0) {
+      for (int x = 0; x < ary1.length; x++) {
+        res[counter] = ary1[x];
+        counter ++;
+      }
+    }
+
+    if (ary2.length != 0) {
+      for (int x = 0; x < ary2.length; x++) {
+        res[counter] = ary2[x];
+        counter ++;
+      }
+    }
+
+    return res;
   }
 
   public static int[] returnCopy(int[]ary) {
