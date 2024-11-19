75aba2d04921c82d1a9b282470e9c5fe4de63884
Anzam Siddek <asiddek50@stuy.edu>
2024-11-19 11:00:17 -0500

    completed day 1 part 1

--- 2024-11-18-advent2016/Main.java
+++ 2024-11-18-advent2016/Main.java
@@ -4,15 +4,66 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    File file = new File("test.txt");//1
-    Scanner input = new Scanner(file);
+    try {
+      File file = new File("test.txt");//1
+      Scanner input = new Scanner(file);
 
-    String[] inputs = input.toString().split(", ");
-    int xcor = 0;
-    int ycor = 0;
+      String[] inputs = input.nextLine().split(", ");
+      int xcor = 0;
+      int ycor = 0;
+      int facing = 0;
 
-    for (int x = 0; x < inputs.length; x++) {
-      System.out.println(inputs[x]);
+      for (int x = 0; x < inputs.length; x++) {
+        String direction = inputs[x].substring(0,1);
+        String dist = (inputs[x].substring(1));
+        int distance = Integer.parseInt(dist);
+
+        if (facing == 0) {
+          if (direction.equals("L")) {
+            xcor -= distance;
+            facing = 3;
+          }
+          else {
+            xcor += distance;
+            facing = 1;
+          }
+        } else if (facing == 1) {
+          if (direction.equals("L")) {
+            ycor += distance;
+            facing = 0;
+          }
+          else {
+            ycor -= distance;
+            facing = 2;
+          }
+        } else if (facing == 2) {
+          if (direction.equals("L")) {
+            xcor += distance;
+            facing = 1;
+          }
+          else {
+            xcor -= distance;
+            facing = 3;
+          }
+        } else if (facing == 3) {
+          if (direction.equals("L")) {
+            ycor -= distance;
+            facing = 2;
+          }
+          else {
+            ycor += distance;
+            facing = 0;
+          }
+        }
+      }
+
+      int res = Math.abs(0 - xcor) + Math.abs(0 - ycor);
+      System.out.println(res);
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return; //you can return from a void function just don't put a value.
     }
   }
 }
