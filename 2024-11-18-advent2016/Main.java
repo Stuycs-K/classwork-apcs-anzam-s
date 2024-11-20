6ebcd7105f0b5f905b20d5779faeef2c7814ca49
Anzam Siddek <asiddek50@stuy.edu>
2024-11-20 10:59:38 -0500

    started working on part 2

--- 2024-11-18-advent2016/Main.java
+++ 2024-11-18-advent2016/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,9 +10,15 @@ public class Main {
       Scanner input = new Scanner(file);
 
       String[] inputs = input.nextLine().split(", ");
+      ArrayList<Integer>xcors = new ArrayList<String>();
+      ArrayList<Integer>ycors = new ArrayList<String>();
+
       int xcor = 0;
       int ycor = 0;
       int facing = 0;
+
+      xcors.add(0);
+      ycors.add(0);
 
       for (int x = 0; x < inputs.length; x++) {
         String direction = inputs[x].substring(0,1);
@@ -20,29 +27,65 @@ public class Main {
 
         if (facing == 0) {
           if (direction.equals("L")) {
-            xcor -= distance;
+            for (int x = 0; x < distance; x++) {
+              xcor --;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              xcors.add(xcor);
+            }
             facing = 3;
           }
           else {
-            xcor += distance;
+            for (int x = 0; x < distance; x++) {
+              xcor ++;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              xcors.add(xcor);
+            }
             facing = 1;
           }
         } else if (facing == 1) {
           if (direction.equals("L")) {
-            ycor += distance;
+            for (int x = 0; x < distance; x++) {
+              ycor ++;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              ycors.add(ycor);
+            }
             facing = 0;
           }
           else {
-            ycor -= distance;
+            for (int x = 0; x < distance; x++) {
+              ycor --;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              ycors.add(ycor);
+            }
             facing = 2;
           }
         } else if (facing == 2) {
           if (direction.equals("L")) {
-            xcor += distance;
+            for (int x = 0; x < distance; x++) {
+              xcor ++;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              xcors.add(xcor);
+            }
             facing = 1;
           }
           else {
-            xcor -= distance;
+            for (int x = 0; x < distance; x++) {
+              xcor --;
+              if (xcors.contains(xcor) && ycors.contains(ycor)) {
+                return (xcor + ", " + ycor);
+              }
+              xcors.add(xcor);
+            }
             facing = 3;
           }
         } else if (facing == 3) {
@@ -56,9 +99,6 @@ public class Main {
           }
         }
       }
-
-      int res = Math.abs(0 - xcor) + Math.abs(0 - ycor);
-      System.out.println(res);
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
