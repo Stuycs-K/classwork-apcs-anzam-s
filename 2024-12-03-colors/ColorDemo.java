491dbdf75e6a6bcd85bfac3d7042d9ad74c6cf93
Anzam Siddek <asiddek50@stuy.edu>
2024-12-03 18:18:34 -0500

    Added moving the cursor

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -3,10 +3,15 @@ public class ColorDemo{
   public static final String HIDE_CURSOR =  "\u001b[?25l";
   public static final String SHOW_CURSOR =  "\u001b[?25h";
 
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
+
   public static void main(String[] args) {
     for(int r = 0; r < 256; r+=16){
       for(int g = 0; g <= 256; g+=16){
         for(int b = 0; b <= 256; b+=16){
+          go(r, g);
           System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
         }
       }
@@ -18,6 +23,17 @@ public class ColorDemo{
 
     System.out.print("\u001b[36m");
     System.out.println("Hello World");
+    System.out.print("\u001b[0m");
+
+    for (int r = 0; r < 16; r++) {
+      for (int g = 0; g < 16; g++) {
+          for (int b = 0; b < 16; b++) {
+              go(r, g);
+              System.out.print("\u001b[38;2;" + (r * 16) + ";" + (g * 16) + ";" + (b * 16) + "m\u2588");
+          }
+      }
+    }  
+    
     System.out.print("\u001b[0m");
   }
 }
